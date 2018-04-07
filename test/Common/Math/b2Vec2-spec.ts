3390239162ea7e190d8911cd3812392034dfa17a
George MacKay-Shore <george@georgeshore.com>
2018-04-07 12:48:09 +0100

    Add test for negate()

--- test/Common/Math/b2Vec2-spec.ts
+++ test/Common/Math/b2Vec2-spec.ts
@@ -3,7 +3,13 @@ import b2Vec2 from '../../../src/Common/Math/b2Vec2';
 
 const expect = chai.expect;
 
-describe('B2Vec2', () => {
+const createRandomVector: () => b2Vec2 = () => {
+  const x = Math.random();
+  const y = Math.random();
+  return new b2Vec2(x, y);
+};
+
+describe('new B2Vec2', () => {
   it('should have a zero x and y component on creation', () => {
     const vec = new b2Vec2();
     expect(vec.x).to.equal(0);
@@ -17,23 +23,31 @@ describe('B2Vec2', () => {
     expect(vec.x).to.equal(x);
     expect(vec.y).to.equal(y);
   });
+});
 
-  it('should be set to {0, 0} after .setZero() call', () => {
+describe('B2Vec2 operations', () => {
+  it('should set vector to {0, 0} after .setZero() call', () => {
     const zeroVec = new b2Vec2();
-    const x = Math.random();
-    const y = Math.random();
-    const vec = new b2Vec2(x, y);
+    const vec = createRandomVector();
     vec.setZero();
     expect(vec.x).to.equal(0);
     expect(vec.y).to.equal(0);
   });
 
-  it('have its x and y set after .set(x, y) call', () => {
+  it('should set vector to {x, y} set after .set(x, y) call', () => {
     const x = Math.random();
     const y = Math.random();
     const vec = new b2Vec2();
     vec.set(x, y);
     expect(vec.x).to.equal(x);
     expect(vec.y).to.equal(y);
+  });
+
+  it('should be negated after a .negate() call', () => {
+    const vec = createRandomVector();
+    const { x, y } = vec;
+    const neg = vec.negate();
+    expect(neg.x).to.equal(-x);
+    expect(neg.y).to.equal(-y);
   });
 });
