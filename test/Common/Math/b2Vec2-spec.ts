24b237727c201a1f586c65bcc9b762caaa8ab3f9
George MacKay-Shore <george@georgeshore.com>
2018-04-07 10:43:59 +0100

    Test to take initial arguments

--- test/Common/Math/b2Vec2-spec.ts
+++ test/Common/Math/b2Vec2-spec.ts
@@ -9,4 +9,12 @@ describe('b2Vec2', () => {
     expect(vec.x).to.equal(0);
     expect(vec.y).to.equal(0);
   });
+
+  it('should take an initial x and y argument on creation', () => {
+    const x = Math.random();
+    const y = Math.random();
+    const vec = new b2Vec2(x, y);
+    expect(vec.x).to.equal(x);
+    expect(vec.y).to.equal(y);
+  });
 });
